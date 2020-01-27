f2b1b851a587a379e5c1a63acce74eeb2ede828c
Ajay Bandi <ajayhunters@gmail.com>
2020-01-27 13:45:05 -0600

    Class demo

---
+++ Car/CarDriver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Car;
+namespace Car
+{
+    class CarDriver
+    {
+        public static void Main(String[] args)
+        {
+            Car c1 = new Car(123, "Benz");
+            Car c2 = new Car(234, "Toyota");
+
+            Console.WriteLine(c1);
+            c1.vin = 989;
+            Console.WriteLine(c1.brand);
+        }
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Car
+{
+    class Car
+    {
+        private int carID;
+        private string carBrand;
+
+        public Car(int carID, string carBrand)
+        {
+            this.carID = carID;
+            this.carBrand = carBrand;
+        }
+
+        public int vin
+        {
+            get { return this.carID;}
+            set {carID = value; }
+        }
+
+        public String brand
+        {
+            get { return this.carBrand; }
+            set { carBrand = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Car ID: " + this.carID + "\nCar Brand: " + this.carBrand;
+        }
+    }///end car
+
+    //class CarDriver
+    //{
+    //    public static void Main(String[] args)
+    //    {
+    //        Car c1 = new Car(123, "Benz");
+    //        Car c2 = new Car(234, "Toyota");
+
+    //        Console.WriteLine(c1);
+    //        c1.vin = 989;
+    //        Console.WriteLine(c1.brand);
+    //    }
+    //}
+}
