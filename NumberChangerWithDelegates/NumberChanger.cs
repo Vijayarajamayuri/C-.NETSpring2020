1e6a4da27f12d134343e03b38a161b417e008a81
Ajay Bandi <ajayhunters@gmail.com>
2020-01-29 12:16:02 -0600

    Calculations using delegates.

---
+++ NumberChangerWithDelegates/NumberChanger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NumberChangerWithDelegates
+{
+    class NumberChanger
+    {
+        static int num = 10;
+
+        public static int add(int n)
+        {
+            num = num + n;
+            return num;
+        }
+
+        public static int sub(int n)
+        {
+            num = num - n;
+            return num;
+        }
+
+        public static int mul(int n)
+        {
+            num = num * n;
+            return num;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            numChangeDel addChanger = new numChangeDel(add);
+            addChanger(20);
+            Console.WriteLine("value of {0}", num);
+
+            numChangeDel subChanger = new numChangeDel(sub);
+            subChanger(20);
+            Console.WriteLine("value of {0}", num);
+        }
+    }
+
+    public delegate int numChangeDel(int val);
+}
