9e7122c087aa62f5cc1852701f68839fabbfbe21
Ajay Bandi <ajayhunters@gmail.com>
2020-02-12 13:37:50 -0600

    Testing Demo

---
+++ BankAccountDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BankAccountDemo
+{
+    public class BankAccount
+    {
+        private double balance;
+        private string custName;
+
+        public BankAccount(double balnance, string custName)
+        {
+            this.balance = balnance;
+            this.custName = custName;
+        }
+
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+        public string Name
+        {
+            get { return this.custName; }
+            set { this.custName = value; }
+        }
+
+        public double deposit(double amount)
+        {
+            return (balance += amount);
+        }
+
+        public double withdraw(double amount)
+        {
+            double retValue;
+            if (balance >= amount)
+            {
+                retValue = balance - amount;
+            }
+            else
+            {
+                retValue = -99;
+            }
+            return retValue;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
